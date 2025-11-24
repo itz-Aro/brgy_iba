7c28fd47459691679da1928b6d77b83f57063621
Aro <haroldenriquez1109.com>
2025-11-25 03:15:19 +0800

    sdds

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['user'])) {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Barangay Login</title>
+<title>Barangay Logins</title>
 <link rel="stylesheet" href="css\style.css">
 
 <!-- EmailJS -->
